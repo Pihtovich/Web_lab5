2da09393dc78473178e5f2aef3246bae133dcd07
Pihtovich <86502038+Pihtovich@users.noreply.github.com>
2024-10-18 21:17:04 +0200

    Add 1 part of code in index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$mysqli = new mysqli('db', 'root', 'helloworld', 'web');
+if (mysqli_connect_errno()) {
+    printf("Подключение к серверу MySQL невозможно. Код ошибки: %s\n", mysqli_connect_error());
+    exit;
+}
+// Обработка формы
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $mysqli->real_escape_string($_POST['email']);
+    $title = $mysqli->real_escape_string($_POST['title']);
+    $category = $mysqli->real_escape_string($_POST['categories']);
+    $description = $mysqli->real_escape_string($_POST['text']);
+    $mysqli->query("INSERT INTO ad (email, title, description, category) VALUES ('$email', '$title', '$description', '$category')");
+}
+$mas = [];
+if ($result = $mysqli->query('SELECT * FROM ad ORDER BY created DESC')) {
+    while ($row = $result->fetch_assoc()) {
+        $mas[] = $row;
+    }
+    $result->close();
+}
+$mysqli->close();
+?>
